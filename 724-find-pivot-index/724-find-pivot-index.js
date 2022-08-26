00642ce2fde8d7415286d7f6e9197187d8222ca9
Ji <98197466+typingtrex@users.noreply.github.com>
2022-08-26 11:06:27 -0400

    Time: 103 ms (71.89%), Space: 44 MB (93.20%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.js
+++ 724-find-pivot-index/724-find-pivot-index.js
@@ -6,6 +6,38 @@ var pivotIndex = function(nums) {
     let pointer; 
     let leftSum = 0;
     let rightSum = 0;
+    for(let j = 0; j < nums.length; j ++) {
+            rightSum += nums[j];
+        }
+    for (let i = 0; i < nums.length; i++) {
+        pointer = i
+        if(nums[i-1]){
+            leftSum += nums[i -1]; 
+        }
+        rightSum = rightSum - nums[i];
+        if (leftSum === rightSum) {
+            return pointer;
+        }
+        
+    }
+    return -1;
+};
+
+         
+// pseudo code 
+         /*
+         first find the sum 
+         do a for loop to start adding the sum starting from the left side  and check left sum vs right sum
+         -- want it to be equal (without counting pivot index num) 
+         if end of loop and not equal, return -1
+         
+         
+         first attempt: Time Complexity ON^2
+
+var pivotIndex = function(nums) {
+    let pointer; 
+    let leftSum = 0;
+    let rightSum = 0;
     for (let i = 0; i < nums.length; i++) {
         pointer = i
         if(nums[i-1]){
@@ -21,13 +53,4 @@ var pivotIndex = function(nums) {
     }
     return -1;
 };
-
-         
-// pseudo code 
-         /*
-         first find the sum 
-         do a for loop to start adding the sum starting from the left side  and check left sum vs right sum
-         -- want it to be equal (without counting pivot index num) 
-         if end of loop and not equal, return -1
-         
          */
