b56a4f567afb53a31711d33ea8fc713a83fa4176
Ji <98197466+typingtrex@users.noreply.github.com>
2022-09-07 10:53:18 -0400

    Time: 161 ms (7.01%), Space: 48.9 MB (87.86%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.js
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.js
@@ -11,39 +11,41 @@
  * @param {number} val
  * @return {TreeNode}
  */
-// var searchBST = function(root, val) {
+var searchBST = function(root, val) {
    
-//         if(root.val === val) {
-//             console.log("inside equal", root)
-//             return root 
-//         }
+    if (!root) {
+            // console.log(currNode.val, currNode.left, currNode.right)
+            return null;
+        }  
     
-//         if (val < root.val) {
-//             // root = root.left;
+        if(root.val === val) {
+            console.log("inside equal", root)
+            return root 
+        }
+    
+        if (val < root.val) {
+            // root = root.left;
             
-//             return searchBST(root.left, val);
-//         } else {
-//             // root = root.right;
-//             // console.log(root, "inside else")
-//             return searchBST(root.right, val);
-//         }    
+            return searchBST(root.left, val);
+        } else {
+            // root = root.right;
+            // console.log(root, "inside else")
+            return searchBST(root.right, val);
+        }    
     
-//         // if (val !== root.val && !root.left && !root.right) {
-//         //     // console.log(currNode.val, currNode.left, currNode.right)
-//         //     return null;
-//         // }            
+                  
     
     
   
-// };
-
-var searchBST = function(root, val) {
-    if (root === null) return null;
-    if (root.val === val) return root;
-    
-    if (val < root.val) {
-        return searchBST(root.left, val);
-    } else if (val > root.val) {
-        return searchBST(root.right, val);
-    }
 };
+
+// var searchBST = function(root, val) {
+//     if (root === null) return null;
+//     if (root.val === val) return root;
+    
+//     if (val < root.val) {
+//         return searchBST(root.left, val);
+//     } else if (val > root.val) {
+//         return searchBST(root.right, val);
+//     }
+// };
