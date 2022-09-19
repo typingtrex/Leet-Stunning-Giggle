37e0e6595682b01859d4fe4b4b3cb9067d0e12f3
Ji <98197466+typingtrex@users.noreply.github.com>
2022-09-19 11:14:57 -0400

    Time: 1567 ms (5.02%), Space: 52.4 MB (5.12%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -7,19 +7,20 @@
     // 2. push as many times as count
 
 var moveZeroes = function(nums) {
-    count = 0;
-    for (let i = 0; i < nums.length; i++) {
+    let length = nums.length;
+    for (let i = 0; i < length; i++) {
         if(nums[i] === 0) {
             nums.splice(i, 1); 
-            count++;
+            nums.push(0)
             i--;
+            length--;
         console.log(nums);
         }
     }
     
-    for(let k = 0; k < count; k++) {
-        nums.push(0)
-    }
+    // for(let k = 0; k < count; k++) {
+    //     nums.push(0)
+    // }
     
     // console.log(nums)
     return nums
