eacd45fe792e25fb4c43c4d694da18fbd1566e97
Ji <98197466+typingtrex@users.noreply.github.com>
2022-09-20 10:07:47 -0400

    Time: 189 ms (8.24%), Space: 42.2 MB (21.21%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -3,25 +3,32 @@
  * @return {number}
  */
 var fib = function(n) {
-    let one = 0
-    let two = 1
+    // ----- dynamic iteration ------
+//     let one = 0
+//     let two = 1
     
-    if(n === 0) {
-        return 0
-    }
-    // console.log("n here", n)
-    for(let i = 0; i < n -1; i++) {
+//     if(n === 0) {
+//         return 0
+//     }
+  
+//     for(let i = 0; i < n -1; i++) {
     
-        let temp = one + two;
+//         let temp = one + two;
     
-        one = two;
-        two = temp;
-        // console.log("n",n);
-        // console.log("one", one);
-        // console.log("two", two)
-    }
+//         one = two;
+//         two = temp;
+     
+//     }
 
-    return two;
+//     return two;
+     // baseCase 
+    if(n === 0) {
+        return 0;
+    }
+    if(n === 1) {
+        return 1;
+    }
     
+    return fib(n -1) + fib(n -2)
 
 };
