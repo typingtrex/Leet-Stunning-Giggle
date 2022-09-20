df3b0700639e042807994c2c1cc7e1c4e0f6b6b1
Ji <98197466+typingtrex@users.noreply.github.com>
2022-09-20 10:05:28 -0400

    Time: 160 ms (15.70%), Space: 44.8 MB (5.59%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    let one = 0
+    let two = 1
+    
+    if(n === 0) {
+        return 0
+    }
+    console.log("n here", n)
+    for(let i = 0; i < n -1; i++) {
+    
+        let temp = one + two;
+    
+        one = two;
+        two = temp;
+        console.log("n",n);
+        console.log("one", one);
+        console.log("two", two)
+    }
+
+    return two;
+    
+
+};
