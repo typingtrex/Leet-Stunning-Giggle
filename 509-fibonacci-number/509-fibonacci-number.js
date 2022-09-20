ba96de893adc2073d4d51216a37c70562a7d2c07
Ji <98197466+typingtrex@users.noreply.github.com>
2022-09-20 13:59:56 -0400

    Time: 120 ms (33.52%), Space: 43.5 MB (5.96%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -2,33 +2,49 @@
  * @param {number} n
  * @return {number}
  */
+
+// 0 1 1 2 3 5 8
+// 0 1 2 3 4 5 6
 var fib = function(n) {
     // ----- dynamic iteration ------
-//     let one = 0
-//     let two = 1
+    let one = 0
+    let two = 1
     
-//     if(n === 0) {
-//         return 0
-//     }
-  
-//     for(let i = 0; i < n -1; i++) {
-    
-//         let temp = one + two;
-    
-//         one = two;
-//         two = temp;
-     
-//     }
-
-//     return two;
-     // baseCase 
     if(n === 0) {
-        return 0;
+        return 0
     }
-    if(n === 1) {
-        return 1;
-    }
+  
+    for(let i = 0; i < n -1; i++) {
     
-    return fib(n -1) + fib(n -2)
+        let temp = one + two;
+        
+        one = two;
+        two = temp;
+        console.log("one", one, "two", two)
+    }
 
+    return two;
+
+    // -----recursive ------
+    //      // baseCase 
+//     if(n === 0) {
+//         return 0;
+//     }
+//     if(n === 1) {
+//         return 1;
+//     }
+    
+//     return fib(n -1) + fib(n -2)
+
+    // ---- recursive with memo -------
+    // , memo = {0:0, 1:1}
+    // base case
+    // if(memo[num]) {
+    //     return memo[num];
+    // } else {
+    //     memo[num];
+    //     fib(num -1, memo);
+    //     memo[num] = memo[num -1] + memo[num -2];
+    //     return memo[num];
+    // }
 };
