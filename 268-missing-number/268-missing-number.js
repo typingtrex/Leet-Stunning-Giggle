080ea80d1e6338d83061a436f2102a602e550c74
Ji <98197466+typingtrex@users.noreply.github.com>
2022-09-22 12:48:01 -0400

    Time: 115 ms (48.40%), Space: 44.5 MB (44.75%) - LeetHub

---
+++ 268-missing-number/268-missing-number.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(array) {
+    let sum = 0;
+    let largest = array.length;	//5
+
+	
+    for (let i = 0; i <= largest; i++) {
+	    sum += i // 15
+    };
+
+    for (let i = 0; i < array.length; i++) {
+	    sum -= array[i]; 
+    }
+
+    return sum; //3
+
+};
