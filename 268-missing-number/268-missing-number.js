e06b60026dd5f96b4561a19b9dfc16fab0b4e8d1
Ji <98197466+typingtrex@users.noreply.github.com>
2022-09-22 13:43:26 -0400

    Time: 113 ms (50.98%), Space: 44.8 MB (27.33%) - LeetHub

--- 268-missing-number/268-missing-number.js
+++ 268-missing-number/268-missing-number.js
@@ -3,17 +3,17 @@
  * @return {number}
  */
 var missingNumber = function(array) {
-    let sum = 0;
+    
     let largest = array.length;	//5
-	
-    for (let i = 0; i <= largest; i++) {
-	    sum += i // 15
-    };
-
+	let sum = largest * ((largest+ 1)/2);
+    // for (let i = 0; i <= largest; i++) {
+    // sum += i // 15
+    // };
+    console.log(largest * ((largest+ 1)/2), sum)
     for (let i = 0; i < array.length; i++) {
 	    sum -= array[i]; 
     }
-
+    
     return sum; //3
     
     // ----- cycle sort method ---- TRY IT!
