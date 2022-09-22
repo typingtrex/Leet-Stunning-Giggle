ebb87df8978265659474f4a51cbcffc349169277
Ji <98197466+typingtrex@users.noreply.github.com>
2022-09-21 22:26:13 -0400

    Time: 102 ms (73.53%), Space: 42.9 MB (43.17%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -19,5 +19,42 @@ var twoSum = function(nums, target) {
         
     }
     return resultArray;
+    
+//     let hashmap = {};
+    
+//     for (let i = 0; i < nums.length; i++) {
+        
+//         hashmap[target - nums[i]] = true;
+//         console.log(hashmap)
+//     }
+    
+//     for(let i = 0; i < nums.length; i++) {
+//         let one = nums[i];
+//         let two = target - one;
+//         console.log(hashmap[one], hashmap[two], target, hashmap)
+        
+//         if(hashmap[two] === ) {
+//             return [i, hashmap[two]]
+//         }
+//     }
 };
 
+// --- prev answ -----
+/*
+let resultArray = [];
+    let hashmap = {}
+    for(let i = 0; i< nums.length; i++) {
+        let currentNum = nums[i]
+        let targetNum = target - currentNum;
+        if(hashmap[targetNum] || hashmap[targetNum] === 0) {
+            resultArray.push(i);
+            resultArray.push(hashmap[targetNum])
+            break;
+        } else {
+            hashmap[currentNum] = i;
+        }
+        
+    }
+    return resultArray;
+
+*/
