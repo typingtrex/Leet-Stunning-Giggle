4a3cbbb3832ac322e818f30e488ce3bd6ca344fd
Ji <98197466+typingtrex@users.noreply.github.com>
2022-10-19 11:43:56 -0400

    Time: 150 ms (61.22%), Space: 49.6 MB (28.90%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -2,15 +2,26 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-var reverseString = function(s) {
-    switchLettersFcn(0, s.length -1, s)
-};
+// var reverseString = function(s) {
+//     switchLettersFcn(0, s.length -1, s)
+// };
 
-let switchLettersFcn = (left, right, s) => {
-    if(left >=right) {
-        return s;
-    }
+// let switchLettersFcn = (left, right, s) => {
+//     if(left >=right) {
+//         return s;
+//     }
     
-    [s[left], s[right]] = [s[right], s[left]];
-    switchLettersFcn(left +1, right -1, s)
-}
+//     [s[left], s[right]] = [s[right], s[left]];
+//     switchLettersFcn(left +1, right -1, s)
+// }
+
+var reverseString = function(s) {
+    let left = 0;
+    let right = s.length -1;
+    
+    while (left < right) {
+        [s[left], s[right]] = [s[right], s[left]];
+        left++;
+        right--;
+    }
+};
