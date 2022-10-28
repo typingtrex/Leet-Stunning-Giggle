89a0b2f92045c28eac210b8a63cc0a5391c1fb02
Ji <98197466+typingtrex@users.noreply.github.com>
2022-10-28 13:27:32 -0400

    Time: 151 ms (7.09%), Space: 46.5 MB (5.55%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,18 +11,35 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    let hashset = new Set();
-    
+  
+    let hashmap = new Map(); // holds ojects and primitive vals
     let currNode = head;
     while(currNode) {
-        // console.log("WHAT is hashset, ", hashset)
-        if(hashset.has(currNode)) {
+        // console.log("WHAT is hashmap, ", hashmap)
+        if(hashmap.has(currNode)) {
             return true;
         } else {
-            hashset.add(currNode);
+            hashmap.set(currNode, true);
         }
         currNode = currNode.next;
     }
     
     return false;
+    
+    
+    //hashset are ugly
+    // let hashset = new Set();
+    
+//     let currNode = head;
+//     while(currNode) {
+//         // console.log("WHAT is hashset, ", hashset)
+//         if(hashset.has(currNode)) {
+//             return true;
+//         } else {
+//             hashset.add(currNode);
+//         }
+//         currNode = currNode.next;
+//     }
+    
+//     return false;
 };
