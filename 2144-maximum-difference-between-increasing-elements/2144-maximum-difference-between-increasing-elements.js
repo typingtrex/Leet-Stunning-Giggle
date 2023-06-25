1d08483ec84ce7818ae6a7e3c47b8cca98aed2ee
Ji <98197466+typingtrex@users.noreply.github.com>
2023-06-25 13:47:25 -0400

    Time: 79 ms (7.38%), Space: 45.7 MB (5.37%) - LeetHub

---
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumDifference = function(nums) {
+    const maxNumsArray = createArrayOfMaxNumRightMost(nums)
+    const minNumsArray = createArrayOfMinNumLeftMost(nums)
+    let difference = -1;
+
+    for (let i = 0; i < maxNumsArray.length; i++) {
+        const max = maxNumsArray[i];
+        const min = minNumsArray[i];
+        const diff = max - min;
+        difference = Math.max(diff, difference);
+    }
+    if (difference === 0) {
+        return -1
+    }
+    return difference;
+};
+
+const createArrayOfMaxNumRightMost = (nums) => {
+    let maxNum = 0;
+    const maxArray = new Array(nums.length).fill(0);
+    
+    for (let i = nums.length - 1; i >= 0; i--) {
+        maxNum = Math.max(maxNum, nums[i]);
+        maxArray[i] = maxNum
+    }
+    return maxArray;
+}
+
+const createArrayOfMinNumLeftMost = (nums) => {
+    let minNum = Infinity;
+    const minArray = new Array(nums.length).fill(0);
+    
+    for (let i = 0; i < nums.length; i++) {
+        minNum = Math.min(minNum, nums[i]);
+        minArray[i] = minNum
+    }
+    console.log(minArray)
+    return minArray;
+}
