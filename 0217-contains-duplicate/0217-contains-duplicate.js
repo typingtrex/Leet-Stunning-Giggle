4acee923556a623ef7c6843230547a32bcbf6ca1
Ji <98197466+typingtrex@users.noreply.github.com>
2023-06-24 22:08:19 -0400

    Time: 197 ms (20.63%), Space: 52 MB (83.29%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,61 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+// ----- most recent solution -----------
+var containsDuplicate = function(nums) {
+    // 1st method: 
+    // sort the array and see if the current num is equal to the next num 
+        // if true return true
+        // after iterating, return false
+        // ---- TIME Complexity ----- O (nLogN)
+        // ------ space complexity  ---- O(1) 
+        nums.sort()
+        for (let i = 0; i < nums.length - 1; i++) {
+            const num1 = nums[i];
+            const num2 = nums[i + 1];
+            if (num1 === num2) {
+                return true
+            }
+        }
+
+        return false
+    // 2nd method: 
+    // iterate and create hashmap
+        // if seen in hashmap then return true
+        // after iteration is complete return false
+        // ----- TIME complexity --- O(N)
+        // ------- SPACE complexity --- O(N)
+
+
+
+}
+
+ // ----- previous solution: ---------
+ /*
+var containsDuplicate = function(nums) {
+    const sortedNums = nums.sort((a, b) => a-b); // time compplexity n(logN)
+    
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === nums[i + 1]) {
+            return true;
+        }
+    }
+    return false;
+    
+    // let seenHashmap = {};
+
+    // for(let i = 0; i < nums.length; i++) {
+    //     if(seenHashmap[nums[i]]) {
+    //         return true;
+    //     } else {
+    //         seenHashmap[nums[i]] = true;
+    //     }
+    // }
+
+    // return false;
+
+    
+};
+*/
