ffbfc956cac3545aaac363fbd7f2409827642c76
Ji <98197466+typingtrex@users.noreply.github.com>
2023-06-27 13:52:57 -0400

    Time: 62 ms (43.7%), Space: 42 MB (77.31%) - LeetHub

---
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.js
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var bstToGst = function(root) {
+    if (root.right === null && root.left === null) {
+        return root
+    }
+
+    
+
+    // base case for recursion is 
+    // if currentNode.right is a leaf node
+
+    // lessons on TREES -----------------
+    /*
+        1. Think RECURSION*** 98%
+        2. Think creating a new recursive function and main func is a wrapper.*** 98%
+            a) what do I want to do at the root node?
+            b) what do I want to do at the key nodes?
+            
+        3. Make your base case: if node is equal to null
+
+        ----------------
+        coding the solution: 
+            1. I want a more global variable = sum that persists after each recursion
+            2. I want to keep going to the right
+        
+    */
+    let currNode = root; 
+   
+    const transformToGST = (currNode, sum) => {
+        // base case
+        if (currNode === null) {
+            return sum
+        }
+        
+        sum = transformToGST(currNode.right, sum)
+        
+        // console.log("before", currNode.val)
+
+        sum += currNode.val // updates the total sum including currNode
+        currNode.val = sum // updates currNode value
+        // console.log(currNode.val)
+
+        sum = transformToGST(currNode.left, sum)
+        return sum
+    }
+
+    transformToGST(currNode, 0)
+    return root
+};
