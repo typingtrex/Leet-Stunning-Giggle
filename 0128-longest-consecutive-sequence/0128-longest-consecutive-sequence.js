9d9e234b65627183859698fe5061c6a7585b4ccf
Ji <98197466+typingtrex@users.noreply.github.com>
2023-07-04 16:23:33 -0400

    Time: 537 ms (24.69%), Space: 62.2 MB (71.26%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -0,0 +1,49 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    // if nums array is empty;
+    if (nums.length === 0) {
+        return 0;
+    }
+    let maxLength = 0;
+    let set = new Set();
+
+    nums.forEach(n => {
+        set.add(n);
+    });
+
+    for(let n of nums){
+        if(!set.has(n-1)){
+            let length = 0;
+            let currNum = n;
+
+            while(set.has(currNum)){
+                currNum++;
+                length++;
+            }
+
+            maxLength = Math.max(maxLength, length)
+        }
+    }
+
+    return maxLength;
+/*
+    // ------ TAKES TOO LONG ------- O (MAX - MIN) TIME COMPLEXITY
+    // iterate once more through the min and max nums
+    // 1, 2, 3, 4
+    for (let i = min; i <= max; i++) {
+        currNum = i;
+        if (mapOfNums[currNum]) {
+            count++;
+            
+        } else {
+            maxConsecutive = Math.max(maxConsecutive, count);
+            count = 0;
+        }
+        
+    }
+    */
+  
+};
