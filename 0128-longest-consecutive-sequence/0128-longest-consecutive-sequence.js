60868ea19f42d0435b5ab0ec2c26ee1b6a0fa94b
Ji <98197466+typingtrex@users.noreply.github.com>
2023-07-13 14:05:50 -0400

    Time: 541 ms (23.17%), Space: 62.3 MB (75.8%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -10,11 +10,16 @@ var longestConsecutive = function(nums) {
     let maxLength = 0;
     let set = new Set();
 
+    // Create a look up for o(1)
     nums.forEach(n => {
         set.add(n);
     });
 
     for(let n of nums){
+
+        // We want to check the consectutive numbers at the beginning
+        // If it isn't we ignore
+        // Otherwise we figure out how long the consectutive length is.
         if(!set.has(n-1)){
             let length = 0;
             let currNum = n;
@@ -29,6 +34,7 @@ var longestConsecutive = function(nums) {
     }
 
     return maxLength;
+
 /*
     // ------ TAKES TOO LONG ------- O (MAX - MIN) TIME COMPLEXITY
     // iterate once more through the min and max nums
