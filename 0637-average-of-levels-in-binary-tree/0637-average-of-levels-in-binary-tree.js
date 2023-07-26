129d56fbe04228cdf80e1f416a8f933ea808002d
Ji <98197466+typingtrex@users.noreply.github.com>
2023-07-26 14:22:35 -0400

    Time: 68 ms (93.55%), Space: 47.2 MB (60%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.js
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.js
@@ -11,6 +11,46 @@
  * @return {number[]}
  */
 var averageOfLevels = function(root) {
+ const averages = [];
+  const queue = [root];
+  let sum;
+  let totalValues;
+  
+  while (queue.length) {
+    totalValues = queue.length;
+    sum = 0
+    // adding the children and removing current level values
+    for (let i = 0; i < totalValues; i++) {
+      const currNode = queue[0]
+      if (currNode.left) {
+        queue.push(currNode.left)
+      } 
+
+      if (currNode.right) {
+        queue.push(currNode.right)
+      } 
+
+      sum += currNode.val;
+      queue.shift();
+    }
+
+    // removing the current level values
+    // for (let i = 0; i < totalValues; i++) {
+    //   const currVal = queue[0];
+    //   sum += currVal;
+    //   queue.shift();
+    // }
+    averages.push(sum/totalValues)
+  }
+
+  return averages;
+
+}
+ /*
+ // previous answer: 
+
+var averageOfLevels = function(root) {
+
     const queue = [root];
     const averages = [];
 
@@ -35,3 +75,5 @@ var averageOfLevels = function(root) {
     }
     return averages
 };
+
+*/
