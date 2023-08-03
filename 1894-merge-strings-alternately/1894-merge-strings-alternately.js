66eb50c1ff3291dd03b5423694be1ca948ae2208
Ji <98197466+typingtrex@users.noreply.github.com>
2023-08-03 00:46:39 -0400

    Time: 55 ms (76.27%), Space: 42.6 MB (25.83%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.js
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.js
@@ -12,9 +12,9 @@ var mergeAlternately = function(word1, word2) {
     
     while (index < word1.length || index < word2.length) {
         if (index < word1.length && index < word2.length) {
-            console.log("merged, word1, word2", mergedString, word1[index], word2[index])
+            // console.log("merged, word1, word2", mergedString, word1[index], word2[index])
             mergedString += word1[index] + word2[index]
-            console.log("after merged, word1, word2", mergedString, word1[index], word2[index])
+            // console.log("after merged, word1, word2", mergedString, word1[index], word2[index])
         } else if (index < word1.length && index >= word2.length) {
             mergedString += word1[index];
         } else if (index < word2.length && index >= word1.length) {
