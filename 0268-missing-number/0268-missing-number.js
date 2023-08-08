27d3cbe434bc44368a23cc566468338833164f47
Ji <98197466+typingtrex@users.noreply.github.com>
2023-08-07 22:52:25 -0400

    Time: 57 ms (88.67%), Space: 44.5 MB (42.19%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,7 +3,24 @@
  * @return {number}
  */
 var missingNumber = function(array) {
-    let sum = 0; 
+    const greatestNum = array.length;
+    let sum = 0;
+    // total sum
+    for (let i = 0; i <= greatestNum; i++) {
+        sum += i;
+    }
+    // find missing num
+    for (let i = 0; i < array.length; i++) {
+        sum -= array[i]
+    }
+
+    return sum;
+};
+
+// older attempt: 
+/*
+var missingNumber = function(array) {
+    // let sum = 0; 
     let largest = array.length;	//5
 	// let sum = largest * ((largest+ 1)/2);
     for (let i = 0; i <= largest; i++) {
@@ -18,3 +35,4 @@ var missingNumber = function(array) {
     
     // ----- cycle sort method ---- TRY IT!
 };
+*/
