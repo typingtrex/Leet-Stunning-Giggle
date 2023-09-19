a8dd6ad2f37b259b1aadddfb4ea473d31ec47c3b
Ji <98197466+typingtrex@users.noreply.github.com>
2023-09-19 19:13:01 -0400

    Time: 55 ms (30.36%), Space: 42.2 MB (12.15%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+// depth first search (from 0 to 5)
+// to eliminate decision trees O(N)
+// caching result -- memoization 
+// gonna do bottomUP 
+/*
+    
+*/
+var climbStairs = function(n) {
+    let one = 1;
+    let two = 1;
+    
+    for (let i = 0; i < n; i++) {
+       
+        let temp = one;
+        one += two;
+        two = temp;
+        // console.log("hi", one, two)
+    }
+    console.log(two)
+    return two
+};
+
