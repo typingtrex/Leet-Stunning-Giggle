8da6fba8e4b6999e68200ad3855220872712fa02
Ji <98197466+typingtrex@users.noreply.github.com>
2023-11-06 11:20:46 -0500

    Time: 59 ms (33.77%), Space: 44.5 MB (29.12%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -9,48 +9,15 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-
-// example:
-// 1 <- 2  3 -> null
-// null <- 1 <- 2 <- 3
-
-// var reverseList = function(head) {
-//     if(head === null || head.next === null) {
-//         return head
-//     }
-//     let prevNode = null;
-//     let currNode = head; //1
-//     let nextNode = currNode.next; //2
-//     while(currNode) {
-//         currNode.next = prevNode; // 1 points to null
-//         prevNode = currNode; //1
-//         currNode = nextNode; //2
-//         if(nextNode.next) {
-//           nextNode = nextNode.next; //3   
-//         }    
-//     }
-    
-//     return prevNode;
-    
-    
-// };
-
 var reverseList = function(head) {
-    if(head === null || head.next === null) {
-        return head
+    let previousNode = null;
+    let currentNode = head; //1
+    let nextNode;
+    while (currentNode) {
+        nextNode = currentNode.next //2
+        currentNode.next = previousNode; // null <- 1
+        previousNode = currentNode; // prev = 1
+        currentNode = nextNode; // 2
     }
-    let prevNode = null;
-    let currNode = head; //1
-    let nextNode //2
-    while(currNode) {
-        nextNode = currNode.next;
-        currNode.next = prevNode; // 1 points to null
-        prevNode = currNode; //1
-        currNode = nextNode; //2
-         
-    }
-    
-    return prevNode;
-    
-    
+    return previousNode
 };
